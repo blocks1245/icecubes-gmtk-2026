4aeb9c44321d009e0c2fec59e9970936ce39521e
Cygnus the rock <cygnustherock@gmail.com>
2026-07-23 00:04:50 +0200

    temp

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -2,12 +2,20 @@ extends Node2D
 
 @onready var player: CharacterBody2D = $Player
 @onready var game_camera: Camera2D = $GameCamera
+@onready var fade: AnimationPlayer = $Fade
 
 const CENTER = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	get_tree().paused = true
+	fade.play("Fade_from_black")
+	await fade.animation_finished
+	
 	game_camera.position.x = ProjectSettings.get_setting("display/window/size/viewport_width") / 2
+	fade.play("countdown")
+	await fade.animation_finished
+	get_tree().paused = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Scripts/victory_collider.gd
+++ Scripts/victory_collider.gd
@@ -1,14 +1,16 @@
 extends Node2D
-
+var starttime
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Area2D.connect("body_entered", Callable(self, "WinLevel"))
+	starttime = Time.get_ticks_msec()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
 func WinLevel(_body: Node2D):
+	$Label.text = "YOU WIN\n level beaten in " + str(float((Time.get_ticks_msec() - starttime)/1000)) + " seconds"
 	$Label.visible = true
 	get_tree().paused = true
