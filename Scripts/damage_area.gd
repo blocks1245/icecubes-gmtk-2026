a057431bd4d5ba5396b42cd0fcf71878305c2091
Cygnus the rock <cygnustherock@gmail.com>
2026-07-24 15:03:57 +0200

    Added primitve UI and damage boxes work

--- Scripts/damage_area.gd
+++ Scripts/damage_area.gd
@@ -8,9 +8,7 @@ class_name damage_area
 ##DAMAGE AREA SIZE
 @export var size: Vector2
 
-func _ready() -> void:
-	collision_shape_2d.size = size
-
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		Signals.KillPlayer.emit()
+		get_tree().paused = true
